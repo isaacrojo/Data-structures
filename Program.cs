a355f4239b5d1a053411d7a8d01e2fafba481b57
isaacrojo <60981406+isaacrojo@users.noreply.github.com>
2020-03-05 18:30:51 -0700

    Práctica usando List<>

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Data_structures
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {        //lista contenedora de elementos
+                // Generics: <>
+            List<string> movieNames = new List<string>();
+
+            movieNames.Add("Pulp Fiction");
+            movieNames.Add("Harry Potter");
+            movieNames.Add("Pacific Rim");
+
+            Console.WriteLine(movieNames.[1]);
+
+        }
+    }
+}
